a5ee5d3a3ec19b68109cf55a514fc4c7da42095a
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-07-16 08:28:56 +0200

    Read from file

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module spin
 
 go 1.18
+
+require (
+	github.com/hpcloud/tail v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220702020025-31831981b65f // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	_ = readFile.Close()
+
+	for _, line := range fileLines {
+		fmt.Println(line)
+	}
+
+	fmt.Println(fileLines)
+
+	//t, _ := tail.TailFile("/Users/E0O/.dotfiles/local.log", tail.Config{Follow: true})
+	//for line := range t.Lines {
+	//	fmt.Println(line.Text)
+	//}
 }
