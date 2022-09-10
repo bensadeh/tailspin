80fd957bea614df0121e1e8d78a4c1b12940ae23
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-09-10 14:17:50 +0200

    Revert "Move struct to core package"
    This reverts commit 4b5ae86010edbaa4bc1c0193ec3e74a489ecb3d9.

--- styling/theme.go
+++ styling/theme.go
@@ -4,14 +4,31 @@ import (
 	_ "embed"
 	"gopkg.in/yaml.v2"
 	"log"
-	"spin/core"
 )
 
 //go:embed defaults/defaults.yaml
 var defaultTheme []byte
 
-func GetTheme() *core.Theme {
-	theme := core.Theme{}
+type Theme struct {
+	SearchAndReplace  []SearchAndReplace  `yaml:"search_and_replace"`
+	RegularExpression []RegularExpression `yaml:"regular_expression"`
+	Date              Date                `yaml:"date"`
+}
+type SearchAndReplace struct {
+	Keywords []string `yaml:"keywords"`
+	Fg       string   `yaml:"fg"`
+}
+type RegularExpression struct {
+	Regexp []string `yaml:"regexp"`
+	Fg     string   `yaml:"fg"`
+}
+type Date struct {
+	Regexp []string `yaml:"regexp"`
+	Fg     string   `yaml:"fg"`
+}
+
+func GetTheme() *Theme {
+	theme := Theme{}
 
 	err := yaml.Unmarshal(defaultTheme, &theme)
 	if err != nil {
