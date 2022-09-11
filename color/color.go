35529d93ab26a309af6c9abbe2d59fc3b71ddfd0
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-09-11 09:44:51 +0200

    Add color package

---
+++ color/color.go
@@ -0,0 +1,17 @@
+package color
+
+const (
+	Reset     = "\033[0m"
+	Bold      = "\033[1m"
+	Faint     = "\033[2m"
+	Italic    = "\033[3m"
+	Underline = "\033[4m"
+	Reverse   = "\033[7m"
+	FgBlack   = "\033[30m"
+	FgRed     = "\033[31m"
+	FgGreen   = "\033[32m"
+	FgYellow  = "\033[33m"
+	FgBlue    = "\033[34m"
+	FgMagenta = "\033[35m"
+	FgCyan    = "\033[36m"
+)
