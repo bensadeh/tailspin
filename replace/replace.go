594196fda061ff68eb254ce3eef42999e2bfd428
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-09-13 18:29:02 +0200

    Write replace package

--- replace/replace.go
+++ replace/replace.go
@@ -1,6 +1,48 @@
 package replace
 
-func searchAndReplaceInBetweenTokens(text, leftToken, RightToken string) string {
+import (
+	"log"
+	"strings"
+)
 
-	return ""
+func searchAndReplaceInBetweenTokens(leftToken, rightToken, s, old, new string) string {
+	leftTokenCount := strings.Count(s, leftToken)
+	rightTokenCount := strings.Count(s, rightToken)
+
+	if leftTokenCount != rightTokenCount || leftTokenCount == 0 || leftToken == "" || rightToken == "" {
+		return s
+	}
+
+	newString := ""
+
+	leftTokenSplit := strings.Split(s, leftToken)
+	for i, s2 := range leftTokenSplit {
+		isOnLastItem := len(leftTokenSplit) == i+1
+
+		newS2 := searchAndReplaceToTheLeftOfToken(rightToken, s2, old, new)
+
+		if isOnLastItem {
+			newString += newS2
+			continue
+		}
+
+		newString += newS2 + leftToken
+
+	}
+
+	return newString
+}
+
+func searchAndReplaceToTheLeftOfToken(token, s, old, new string) string {
+	split := strings.Split(s, token)
+
+	if len(split) == 1 {
+		return s
+	}
+
+	if len(split) != 2 {
+		log.Fatalln("Unexpected token count")
+	}
+
+	return strings.ReplaceAll(split[0], old, new) + token + split[1]
 }
