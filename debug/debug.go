74cad1bde654ce3f78f8d0906df310d6011a5616
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-09-17 09:04:02 +0200

    Add another log file for debugging

--- debug/debug.go
+++ debug/debug.go
@@ -8,12 +8,17 @@ var d1 string
 //go:embed 2.log
 var d2 string
 
+//go:embed 3.log
+var d3 string
+
 func GetDebugFile(i int) string {
 	switch i {
 	case 1:
 		return d1
 	case 2:
 		return d2
+	case 3:
+		return d3
 	default:
 		return ""
 	}
