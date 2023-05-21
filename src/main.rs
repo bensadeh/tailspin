8698fb05f32dcb833da3f46ebff1beacb4eb9afe
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-05-21 07:46:30 +0200

    Test pipe stdin to less

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let text = "This is the text to be displayed by less.";
+
+    run_less_with_input(text);
+
+    println!("it works!")
+}
+
+fn run_less_with_input(input: &str) {
+    let mut less_child = Command::new("less")
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute 'less' command");
+
+    if let Some(less_stdin) = less_child.stdin.as_mut() {
+        less_stdin
+            .write_all(input.as_bytes())
+            .expect("Failed to write to 'less' stdin");
+    }
+
+    less_child
+        .wait()
+        .expect("Failed to wait for 'less' command");
 }
