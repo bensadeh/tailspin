00ded861b830352421cfdce8a6b73f049243190b
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-05-28 16:53:44 +0200

    Highlight quotes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use linemux::MuxedLines;
 use regex::Regex;
+use std::collections::VecDeque;
 use std::io;
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
@@ -40,8 +41,9 @@ where
 
     while let Ok(Some(line)) = lines.next_line().await {
         let highlighted_string = highlight_numbers_in_blue(line.line());
+        let highlighted_string2 = highlight_quotes(highlighted_string.as_str());
 
-        writeln!(output_writer, "{}", highlighted_string)?;
+        writeln!(output_writer, "{}", highlighted_string2)?;
         output_writer.flush()?;
     }
 
@@ -71,4 +73,44 @@ fn highlight_numbers_in_blue(input: &str) -> String {
     });
 
     highlighted.into_owned()
+}
+
+fn highlight_quotes(input: &str) -> String {
+    let yellow_color = "\x1b[33m"; // Yellow
+    let reset_color = "\x1b[0m";
+
+    let re_string = Regex::new(r#"'[^']*'|"[^"]*""#).unwrap();
+    let re_color_reset = Regex::new(r"\x1b\[0m").unwrap();
+
+    // Check if there's an uneven amount of quotes
+    if re_string.find_iter(input).count() % 2 != 0 {
+        return input.to_owned();
+    }
+
+    let mut result = String::new();
+    let mut start = 0;
+    for mat in re_string.find_iter(input) {
+        result.push_str(&input[start..mat.start()]); // Before the match
+        let string = &input[mat.start()..mat.end()]; // The matched string
+
+        // Apply yellow color to string part
+        result.push_str(yellow_color);
+
+        // If the string part contains a color reset, replace it with itself plus yellow color
+        let string = if re_color_reset.is_match(string) {
+            re_color_reset
+                .replace_all(string, format!("{}{}", reset_color, yellow_color).as_str())
+                .to_string()
+        } else {
+            string.to_string()
+        };
+
+        result.push_str(&string);
+        result.push_str(reset_color);
+
+        start = mat.end(); // Continue after the match
+    }
+    result.push_str(&input[start..]); // After the last match
+
+    result
 }
