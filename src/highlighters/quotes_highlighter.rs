f0b03b6a88b7639bd75f5fae5f40648231373b9d
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-06-18 08:02:16 +0200

    Refactor

--- src/highlighters/quotes_highlighter.rs
+++ src/highlighters/quotes_highlighter.rs
@@ -7,13 +7,13 @@ pub fn highlight_quotes(color: String) -> HighlightFn {
 
 enum State {
     InsideQuote {
-        color: String,
+        color_inside_quote: String,
         potential_reset_code: String,
     },
     OutsideQuote,
 }
 
-fn highlight_string(color: &String, input: &str) -> String {
+fn highlight_string(color: &str, input: &str) -> String {
     const RESET: &str = "\x1b[0m";
     const QUOTE_SYMBOL: char = '"';
 
@@ -28,30 +28,30 @@ fn highlight_string(color: &String, input: &str) -> String {
     for ch in input.chars() {
         state = match (ch, &mut state) {
             (QUOTE_SYMBOL, InsideQuote { .. }) => {
-                output.push_str(&color);
+                output.push_str(color);
                 output.push(ch);
                 output.push_str(RESET);
                 OutsideQuote
             }
             (QUOTE_SYMBOL, OutsideQuote) => {
-                output.push_str(&color);
+                output.push_str(color);
                 output.push(ch);
                 InsideQuote {
-                    color: color.clone(),
+                    color_inside_quote: color.to_string(),
                     potential_reset_code: String::new(),
                 }
             }
             (
                 _,
                 InsideQuote {
-                    color,
+                    color_inside_quote: color,
                     ref mut potential_reset_code,
                 },
             ) => {
                 potential_reset_code.push(ch);
                 if potential_reset_code.as_str() == RESET {
                     output.push_str(potential_reset_code);
-                    output.push_str(&color);
+                    output.push_str(color);
                     potential_reset_code.clear();
                 } else if !RESET.starts_with(potential_reset_code.as_str()) {
                     output.push_str(potential_reset_code);
