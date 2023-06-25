5d0c9221cbdecc836d07447bfcc1e012a0243960
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-06-25 18:24:52 +0200

    Update date regexp

--- src/highlighters/date.rs
+++ src/highlighters/date.rs
@@ -12,8 +12,24 @@ pub fn highlight(style: &Style) -> HighlightFn {
 
 fn highlight_dates(color: &str, input: &str) -> String {
     let date_regex = Regex::new(
-        r"\b(\d{4}-\d{2}-\d{2}(?:(?:\s|T)\d{2}:\d{2}:\d{2}([.,]\d+)?Z?)?|\d{2}:\d{2}:\d{2}([.,]\d+)?)\b"
-    ).expect("Invalid regex pattern");
+        r"(?x)                 # Enable comments and whitespace insensitivity
+    \b                         # Word boundary, ensures we are at the start of a date/time string
+    (                          # Begin capturing group for the entire date/time string
+        \d{4}-\d{2}-\d{2}      # Matches date in the format: yyyy-mm-dd
+        (?:                    # Begin non-capturing group for the time and timezone
+            (?:\s|T)           # Matches either a whitespace or T (separator between date and time)
+            \d{2}:\d{2}:\d{2}  # Matches time in the format: hh:mm:ss
+            ([.,]\d+)?         # Optionally matches fractional seconds
+            (Z|[+-]\d{2})?     # Optionally matches Z or timezone offset in the format: +hh or -hh
+        )?                     # End non-capturing group for the time and timezone
+        |                      # Alternation, matches either the pattern above or  below
+        \d{2}:\d{2}:\d{2}      # Matches time in the format: hh:mm:ss
+        ([.,]\d+)?             # Optionally matches fractional seconds
+    )                          # End capturing group for the entire date/time string
+    \b                         # Word boundary, ensures we are at the end of a date/time string
+    ",
+    )
+    .expect("Invalid regex pattern");
 
     let highlighted = date_regex.replace_all(input, |caps: &regex::Captures<'_>| {
         format!("{}{}{}", color, &caps[0], color::RESET)
