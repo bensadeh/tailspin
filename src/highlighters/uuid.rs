baea796934d4ecfd43254f0680919639e18c3e47
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-06-26 16:00:30 +0200

    Move regexp creation up

--- src/highlighters/uuid.rs
+++ src/highlighters/uuid.rs
@@ -11,8 +11,20 @@ pub fn highlight(segment: &Style, separator: &Style) -> HighlightFn {
     let separator_color = to_ansi(separator);
 
     Box::new(move |input: &str, line_info: &LineInfo| -> String {
-        highlight_uuids(&segment_color, &separator_color, input, line_info)
+        highlight_uuids(
+            &segment_color,
+            &separator_color,
+            input,
+            line_info,
+            &uuid_regex(),
+        )
     })
+}
+
+fn uuid_regex() -> Regex {
+    Regex::new(
+        r"(\b[0-9a-fA-F]{8}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{12}\b)"
+    ).expect("Invalid regex pattern")
 }
 
 fn highlight_uuids(
@@ -20,12 +32,9 @@ fn highlight_uuids(
     separator_color: &str,
     input: &str,
     _line_info: &LineInfo,
+    uuid_regex: &Regex,
 ) -> String {
-    let uuid_regex = Regex::new(
-        r"(\b[0-9a-fA-F]{8}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{4}\b)(-)(\b[0-9a-fA-F]{12}\b)"
-    ).expect("Invalid regex pattern");
-
-    highlight_with_awareness(input, &uuid_regex, |caps: &Captures<'_>| {
+    highlight_with_awareness(input, uuid_regex, |caps: &Captures<'_>| {
         let mut output = String::new();
         for i in 1..caps.len() {
             if &caps[i] == "-" {
@@ -48,13 +57,20 @@ mod tests {
             dashes: 0,
             dots: 0,
             slashes: 0,
+            double_quotes: 0,
         };
 
         let uuid = "550e8400-e29b-41d4-a716-446655440000";
         let segment_color = "\x1b[31m"; // ANSI color code for red
         let separator_color = "\x1b[32m"; // ANSI color code for green
 
-        let highlighted = highlight_uuids(segment_color, separator_color, uuid, line_info);
+        let highlighted = highlight_uuids(
+            segment_color,
+            separator_color,
+            uuid,
+            line_info,
+            &uuid_regex(),
+        );
 
         let expected = format!(
             "{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
@@ -95,13 +111,20 @@ mod tests {
             dashes: 0,
             dots: 0,
             slashes: 0,
+            double_quotes: 0,
         };
 
         let text = "this is a test string with no uuid";
         let segment_color = "\x1b[31m";
         let separator_color = "\x1b[32m";
 
-        let highlighted = highlight_uuids(segment_color, separator_color, text, line_info);
+        let highlighted = highlight_uuids(
+            segment_color,
+            separator_color,
+            text,
+            line_info,
+            &uuid_regex(),
+        );
 
         // The input string does not contain a UUID, so it should be returned as-is
         assert_eq!(highlighted, text);
