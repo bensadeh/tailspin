39946da0931810efa5c975c774b36460c5dae802
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-06-26 15:49:43 +0200

    Move regex out of the highlight function

--- src/highlighters/keyword.rs
+++ src/highlighters/keyword.rs
@@ -7,15 +7,20 @@ use regex::Regex;
 
 pub fn highlight(keyword: String, style: &Style) -> HighlightFn {
     let color = to_ansi(style);
+    let keyword = regex::escape(&keyword);
+    let keyword_regex = Regex::new(&format!(r"\b{}\b", keyword)).expect("Invalid regex pattern");
 
     Box::new(move |input: &str, line_info: &LineInfo| -> String {
-        highlight_keywords(&keyword, &color, input, line_info)
+        highlight_keywords(&keyword, &color, input, line_info, &keyword_regex)
     })
 }
 
-fn highlight_keywords(keyword: &str, color: &str, input: &str, _line_info: &LineInfo) -> String {
-    let keyword = regex::escape(keyword);
-    let keyword_regex = Regex::new(&format!(r"\b{}\b", keyword)).expect("Invalid regex pattern");
-
-    highlight_utils::highlight_with_awareness_replace_all(color, input, &keyword_regex)
+fn highlight_keywords(
+    _keyword: &str,
+    color: &str,
+    input: &str,
+    _line_info: &LineInfo,
+    keyword_regex: &Regex,
+) -> String {
+    highlight_utils::highlight_with_awareness_replace_all(color, input, keyword_regex)
 }
