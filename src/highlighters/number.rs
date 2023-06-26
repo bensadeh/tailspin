ea3ede94907ee9285234080e7be2337b0b5ebdcd
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-06-26 16:24:30 +0200

    Move regexp up

--- src/highlighters/number.rs
+++ src/highlighters/number.rs
@@ -9,22 +9,29 @@ pub fn highlight(style: &Style) -> HighlightFn {
     let color = to_ansi(style);
 
     Box::new(move |input: &str, line_info: &LineInfo| -> String {
-        highlight_numbers(&color, input, line_info)
+        highlight_numbers(&color, input, line_info, &number_regex())
     })
 }
 
-fn highlight_numbers(color: &str, input: &str, _line_info: &LineInfo) -> String {
-    let number_regex = Regex::new(
-        r"(?x)   # Enable comments and whitespace insensitivity
-    \b           # Word boundary, ensures we are at the start of a number
-    \d+          # Matches one or more digits
-    (\.          # Start a group to match a decimal part
-    \d+          # Matches one or more digits after the dot
-    )?           # The decimal part is optional
-    \b           # Word boundary, ensures we are at the end of a number
-    ",
+fn number_regex() -> Regex {
+    Regex::new(
+        r"(?x)       # Enable comments and whitespace insensitivity
+        \b           # Word boundary, ensures we are at the start of a number
+        \d+          # Matches one or more digits
+        (\.          # Start a group to match a decimal part
+        \d+          # Matches one or more digits after the dot
+        )?           # The decimal part is optional
+        \b           # Word boundary, ensures we are at the end of a number
+        ",
     )
-    .expect("Invalid regex pattern");
+    .expect("Invalid regex pattern")
+}
 
-    highlight_utils::highlight_with_awareness_replace_all(color, input, &number_regex)
+fn highlight_numbers(
+    color: &str,
+    input: &str,
+    _line_info: &LineInfo,
+    number_regex: &Regex,
+) -> String {
+    highlight_utils::highlight_with_awareness_replace_all(color, input, number_regex)
 }
