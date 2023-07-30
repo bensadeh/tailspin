7459d9057aea62762cb909969f69a380eadbab5f
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-07-30 16:27:02 +0200

    Properly send signal

--- src/reader/stdin.rs
+++ src/reader/stdin.rs
@@ -1,23 +1,19 @@
 use crate::reader::AsyncLineReader;
 use async_trait::async_trait;
 use tokio::io;
-use tokio::io::{AsyncBufReadExt, BufReader, Stdin};
+use tokio::io::{AsyncBufReadExt, BufReader, Lines, Stdin};
 use tokio::sync::oneshot::Sender;
 
 pub struct StdinReader {
-    reader: BufReader<Stdin>,
+    reader: Lines<BufReader<Stdin>>,
+    reached_eof_tx: Option<Sender<()>>,
 }
 
 impl StdinReader {
     pub fn get_reader(reached_eof_tx: Option<Sender<()>>) -> Box<dyn AsyncLineReader + Send> {
-        if let Some(reached_eof) = reached_eof_tx {
-            reached_eof
-                .send(())
-                .expect("Failed sending EOF signal to oneshot channel");
-        }
-
         Box::new(StdinReader {
-            reader: BufReader::new(tokio::io::stdin()),
+            reader: BufReader::new(tokio::io::stdin()).lines(),
+            reached_eof_tx,
         })
     }
 }
@@ -25,12 +21,17 @@ impl StdinReader {
 #[async_trait]
 impl AsyncLineReader for StdinReader {
     async fn next_line(&mut self) -> io::Result<Option<String>> {
-        let mut buf = String::new();
-        let bytes_read = self.reader.read_line(&mut buf).await?;
-        if bytes_read == 0 {
-            Ok(None)
-        } else {
-            Ok(Some(buf))
+        match self.reader.next_line().await {
+            Ok(Some(line)) => Ok(Some(line)),
+            Ok(None) => {
+                if let Some(reached_eof) = self.reached_eof_tx.take() {
+                    reached_eof
+                        .send(())
+                        .expect("Failed sending EOF signal to oneshot channel");
+                }
+                Ok(None)
+            }
+            Err(e) => Err(e),
         }
     }
 }
