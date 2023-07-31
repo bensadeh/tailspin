0ff9bb90cc2d264cde711a2e958e99ac62e9bc8c
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-07-31 08:45:34 +0200

    Handle non-utf8 streams from stdin

--- src/reader/stdin.rs
+++ src/reader/stdin.rs
@@ -1,18 +1,18 @@
 use crate::reader::AsyncLineReader;
 use async_trait::async_trait;
 use tokio::io;
-use tokio::io::{AsyncBufReadExt, BufReader, Lines, Stdin};
+use tokio::io::{stdin, AsyncBufReadExt, BufReader, Stdin};
 use tokio::sync::oneshot::Sender;
 
 pub struct StdinReader {
-    reader: Lines<BufReader<Stdin>>,
+    reader: BufReader<Stdin>,
     reached_eof_tx: Option<Sender<()>>,
 }
 
 impl StdinReader {
     pub fn get_reader(reached_eof_tx: Option<Sender<()>>) -> Box<dyn AsyncLineReader + Send> {
         Box::new(StdinReader {
-            reader: BufReader::new(tokio::io::stdin()).lines(),
+            reader: BufReader::new(stdin()),
             reached_eof_tx,
         })
     }
@@ -21,15 +21,24 @@ impl StdinReader {
 #[async_trait]
 impl AsyncLineReader for StdinReader {
     async fn next_line(&mut self) -> io::Result<Option<String>> {
-        match self.reader.next_line().await {
-            Ok(Some(line)) => Ok(Some(line)),
-            Ok(None) => {
+        let mut buffer = Vec::new();
+        match self.reader.read_until(b'\n', &mut buffer).await {
+            Ok(0) => {
                 if let Some(reached_eof) = self.reached_eof_tx.take() {
                     reached_eof
                         .send(())
                         .expect("Failed sending EOF signal to oneshot channel");
                 }
                 Ok(None)
+            }
+            Ok(_) => {
+                if let Some(last_byte) = buffer.last() {
+                    if *last_byte == b'\n' {
+                        buffer.pop();
+                    }
+                }
+                let line = String::from_utf8_lossy(&buffer);
+                Ok(Some(line.into_owned()))
             }
             Err(e) => Err(e),
         }
