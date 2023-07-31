1ea4ac0623aa98ebb3c88ac2385cbbd043c1c16e
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-07-31 11:28:25 +0200

    Refactor stdin.rs

--- src/reader/stdin.rs
+++ src/reader/stdin.rs
@@ -1,7 +1,7 @@
 use crate::reader::AsyncLineReader;
 use async_trait::async_trait;
 use tokio::io;
-use tokio::io::{stdin, AsyncBufReadExt, BufReader, Stdin};
+use tokio::io::{AsyncBufReadExt, BufReader, Stdin};
 use tokio::sync::oneshot::Sender;
 
 pub struct StdinReader {
@@ -12,35 +12,53 @@ pub struct StdinReader {
 impl StdinReader {
     pub fn get_reader(reached_eof_tx: Option<Sender<()>>) -> Box<dyn AsyncLineReader + Send> {
         Box::new(StdinReader {
-            reader: BufReader::new(stdin()),
+            reader: BufReader::new(tokio::io::stdin()),
             reached_eof_tx,
         })
+    }
+
+    async fn read_bytes_until_newline(&mut self) -> io::Result<Vec<u8>> {
+        let mut buffer = Vec::new();
+
+        self.reader.read_until(b'\n', &mut buffer).await?;
+
+        Ok(buffer)
+    }
+
+    fn strip_newline_character(buffer: Vec<u8>) -> Vec<u8> {
+        let mut buf = buffer;
+
+        if let Some(last_byte) = buf.last() {
+            if *last_byte == b'\n' {
+                buf.pop();
+            }
+        }
+
+        buf
+    }
+
+    fn send_eof_signal(&mut self) {
+        if let Some(reached_eof) = self.reached_eof_tx.take() {
+            reached_eof
+                .send(())
+                .expect("Failed sending EOF signal to oneshot channel");
+        }
     }
 }
 
 #[async_trait]
 impl AsyncLineReader for StdinReader {
     async fn next_line(&mut self) -> io::Result<Option<String>> {
-        let mut buffer = Vec::new();
-        match self.reader.read_until(b'\n', &mut buffer).await {
-            Ok(0) => {
-                if let Some(reached_eof) = self.reached_eof_tx.take() {
-                    reached_eof
-                        .send(())
-                        .expect("Failed sending EOF signal to oneshot channel");
-                }
-                Ok(None)
-            }
-            Ok(_) => {
-                if let Some(last_byte) = buffer.last() {
-                    if *last_byte == b'\n' {
-                        buffer.pop();
-                    }
-                }
-                let line = String::from_utf8_lossy(&buffer);
-                Ok(Some(line.into_owned()))
-            }
-            Err(e) => Err(e),
+        let buffer = self.read_bytes_until_newline().await?;
+
+        if buffer.is_empty() {
+            self.send_eof_signal();
+            return Ok(None);
         }
+
+        let buffer = Self::strip_newline_character(buffer);
+        let line = String::from_utf8_lossy(&buffer).into_owned();
+
+        Ok(Some(line))
     }
 }
