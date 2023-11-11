83377e3c8625b040d27c1d75f42435dd7c78a41c
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-11-11 07:57:39 +0100

    Add integration tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,17 @@
+mod utils;
+
+#[test]
+fn test_binary_with_various_inputs() {
+    let binary_path = utils::build_binary();
+
+    let test_cases = [
+        ("Hello null", "Hello \u{1b}[3;31mnull\u{1b}[0m"),
+        ("Hello world", "Hello world"),
+        ("", ""),
+    ];
+
+    for (input, expected_output) in test_cases {
+        let output = utils::run_binary_with_input(binary_path.clone(), input);
+        assert_eq!(output.trim(), expected_output, "Failed on input: {}", input);
+    }
+}
---
+++ tests/utils.rs
@@ -0,0 +1,28 @@
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+pub fn build_binary() -> PathBuf {
+    Command::new("cargo")
+        .arg("build")
+        .status()
+        .expect("Failed to execute cargo build");
+
+    PathBuf::from("./target/debug/tspin")
+}
+
+pub fn run_binary_with_input(binary_path: PathBuf, input: &str) -> String {
+    let mut child = Command::new(binary_path)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    if let Some(stdin) = child.stdin.as_mut() {
+        stdin.write_all(input.as_bytes()).expect("Failed to write to stdin");
+    }
+
+    let output = child.wait_with_output().expect("Failed to read output");
+
+    String::from_utf8_lossy(&output.stdout).into_owned()
+}
