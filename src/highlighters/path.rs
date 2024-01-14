3b0eaffef1700b375d3f24d5065c4be62124df55
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2024-01-14 09:51:37 +0100

    Clean up path highlighter

--- src/highlighters/path.rs
+++ src/highlighters/path.rs
@@ -1,4 +1,3 @@
-use crate::highlight_utils::highlight_with_awareness;
 use crate::line_info::LineInfo;
 use crate::types::Highlight;
 use nu_ansi_term::Style;
@@ -21,6 +20,12 @@ pub struct PathHighlighter {
     separator: Style,
 }
 
+impl PathHighlighter {
+    pub fn new(segment: Style, separator: Style) -> Self {
+        Self { segment, separator }
+    }
+}
+
 impl Highlight for PathHighlighter {
     fn should_short_circuit(&self, line_info: &LineInfo) -> bool {
         line_info.slashes == 0
@@ -31,88 +36,28 @@ impl Highlight for PathHighlighter {
     }
 
     fn apply(&self, input: &str) -> String {
-        self.highlight_paths(input)
-    }
-}
+        PATH_REGEX
+            .replace_all(input, |caps: &Captures<'_>| {
+                let mut output = String::new();
+                let path = &caps[0];
+                let chars: Vec<_> = path.chars().collect();
 
-impl PathHighlighter {
-    pub fn new(segment: Style, separator: Style) -> Self {
-        Self { segment, separator }
-    }
-
-    fn highlight_paths(&self, input: &str) -> String {
-        highlight_with_awareness(input, &PATH_REGEX, |caps: &Captures<'_>| {
-            let mut output = String::new();
-            let path = &caps[0];
-            let chars: Vec<_> = path.chars().collect();
-
-            // Check if path starts with a valid character and not a double slash
-            if !(chars[0] == '/' || chars[0] == '~' || (chars[0] == '.' && chars.len() > 1 && chars[1] == '/'))
-                || (chars[0] == '/' && chars.len() > 1 && chars[1] == '/')
-            {
-                return path.to_string();
-            }
-
-            for &char in &chars {
-                match char {
-                    '/' => output.push_str(&format!("{}", self.separator.paint(char.to_string()))),
-                    _ => output.push_str(&format!("{}", self.segment.paint(char.to_string()))),
+                // Check if path starts with a valid character and not a double slash
+                if !(chars[0] == '/' || chars[0] == '~' || (chars[0] == '.' && chars.len() > 1 && chars[1] == '/'))
+                    || (chars[0] == '/' && chars.len() > 1 && chars[1] == '/')
+                {
+                    return path.to_string();
                 }
-            }
 
-            output
-        })
+                for &char in &chars {
+                    match char {
+                        '/' => output.push_str(&format!("{}", self.separator.paint(char.to_string()))),
+                        _ => output.push_str(&format!("{}", self.segment.paint(char.to_string()))),
+                    }
+                }
+
+                output
+            })
+            .to_string()
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::color::Fg;
-//
-//     #[test]
-//     fn test_highlight_paths() {
-//         let path = "~/Documents/../user/.";
-//         let segment_style = Style {
-//             fg: Fg::Red,
-//             ..Default::default()
-//         };
-//         let separator_style = Style {
-//             fg: Fg::Green,
-//             ..Default::default()
-//         };
-//
-//         let highlighter = PathHighlighter::new(&segment_style, &separator_style);
-//         let highlighted = highlighter.apply(path);
-//
-//         let expected = path
-//             .chars()
-//             .map(|ch| {
-//                 if ch == '/' {
-//                     format!("{}{}{}", to_ansi(&separator_style), ch, color::RESET)
-//                 } else {
-//                     format!("{}{}{}", to_ansi(&segment_style), ch, color::RESET)
-//                 }
-//             })
-//             .collect::<String>();
-//         assert_eq!(highlighted, expected);
-//     }
-//
-//     #[test]
-//     fn test_highlight_paths_no_path() {
-//         let text = "this is a test string with no path";
-//         let segment_style = Style {
-//             fg: Fg::Red,
-//             ..Default::default()
-//         };
-//         let separator_style = Style {
-//             fg: Fg::Green,
-//             ..Default::default()
-//         };
-//
-//         let highlighter = PathHighlighter::new(&segment_style, &separator_style);
-//         let highlighted = highlighter.apply(text);
-//
-//         assert_eq!(highlighted, text);
-//     }
-// }
