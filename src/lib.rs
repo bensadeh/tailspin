ed3f850fda03f1bd5e4a303a93aeca025d38d906
destifo <estifanosbireda@gmail.com>
2024-02-02 22:14:58 +0300

    feat: add library crate support

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod cli;
+pub mod config;
+pub mod highlight_processor;
+pub mod highlight_utils;
+pub mod highlighters;
+pub mod io;
+pub mod line_info;
+pub mod theme;
+pub mod theme_io;
+pub mod types;
+pub mod keyword;
