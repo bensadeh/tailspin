0b502716a3918b874672bd90ea897066a5b83c81
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2024-10-12 10:44:06 +0200

    Refactor keyword highlighter and update Cargo.lock

--- src/highlighter/mod.rs
+++ src/highlighter/mod.rs
@@ -1,7 +1,10 @@
+mod builtins;
 pub mod groups;
 
+use crate::highlighter::builtins::get_builtin_keywords;
 use crate::highlighter::groups::HighlighterGroups;
 use crate::theme::Theme;
+use inlet_manifold::highlighter::HighlightBuilder;
 use inlet_manifold::*;
 
 pub fn get_highlighter(
@@ -53,18 +56,12 @@ pub fn get_highlighter(
         builder.with_number_highlighter(theme.numbers);
     }
 
-    {
-        let builtin_keywords = get_builtin_keywords(disable_builtin_keywords);
-
-        let keywords = vec![]
-            .into_iter()
-            .chain(theme.keywords)
-            .chain(builtin_keywords)
-            .chain(keyword_configs_from_cli)
-            .collect();
-
-        builder.with_keyword_highlighter(keywords);
-    }
+    add_keywords(
+        theme.keywords,
+        keyword_configs_from_cli,
+        disable_builtin_keywords,
+        &mut builder,
+    );
 
     if !theme.regexes.is_empty() {
         for regex in theme.regexes {
@@ -79,65 +76,20 @@ pub fn get_highlighter(
     builder.build()
 }
 
-fn get_builtin_keywords(disable_builtin_keywords: bool) -> Vec<KeywordConfig> {
-    match disable_builtin_keywords {
-        true => vec![],
-        false => builtin_keywords(),
-    }
-}
+fn add_keywords(
+    keywords_from_toml: Vec<KeywordConfig>,
+    keyword_from_cli: Vec<KeywordConfig>,
+    disable_builtin_keywords: bool,
+    builder: &mut HighlightBuilder,
+) {
+    let builtin_keywords = get_builtin_keywords(disable_builtin_keywords);
 
-fn builtin_keywords() -> Vec<KeywordConfig> {
-    let severity_levels = vec![
-        KeywordConfig {
-            words: vec!["ERROR".to_string()],
-            style: Style::new().fg(Color::Red),
-        },
-        KeywordConfig {
-            words: vec!["WARN".to_string(), "WARNING".to_string()],
-            style: Style::new().fg(Color::Yellow),
-        },
-        KeywordConfig {
-            words: vec!["INFO".to_string()],
-            style: Style::new().fg(Color::White),
-        },
-        KeywordConfig {
-            words: vec!["SUCCESS".to_string(), "DEBUG".to_string()],
-            style: Style::new().fg(Color::Green),
-        },
-        KeywordConfig {
-            words: vec!["TRACE".to_string()],
-            style: Style::new().faint(),
-        },
-    ];
-
-    let rest_keywords = vec![
-        KeywordConfig {
-            words: vec!["GET".to_string()],
-            style: Style::new().fg(Color::Black).on(Color::Green),
-        },
-        KeywordConfig {
-            words: vec!["POST".to_string()],
-            style: Style::new().fg(Color::Black).on(Color::Yellow),
-        },
-        KeywordConfig {
-            words: vec!["PUT".to_string(), "PATCH".to_string()],
-            style: Style::new().fg(Color::Black).on(Color::Magenta),
-        },
-        KeywordConfig {
-            words: vec!["DELETE".to_string()],
-            style: Style::new().fg(Color::Black).on(Color::Red),
-        },
-    ];
-
-    let booleans = [KeywordConfig {
-        words: vec!["null".to_string(), "true".to_string(), "false".to_string()],
-        style: Style::new().fg(Color::Red).italic(),
-    }];
-
-    vec![]
+    let keywords = vec![]
         .into_iter()
-        .chain(severity_levels)
-        .chain(rest_keywords)
-        .chain(booleans)
-        .collect()
+        .chain(builtin_keywords)
+        .chain(keywords_from_toml)
+        .chain(keyword_from_cli)
+        .collect();
+
+    builder.with_keyword_highlighter(keywords);
 }
