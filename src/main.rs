584da26b91e0bf7cbef71b5b80e5a13a0d2f081d
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2024-10-13 10:01:16 +0200

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use rayon::iter::ParallelIterator;
 mod cli;
 mod config;
-mod highlighter_builder;
+mod highlighter;
 mod io;
 mod theme;
 
@@ -13,7 +13,7 @@ use crate::io::presenter::Present;
 use crate::io::reader::AsyncLineReader;
 use crate::io::writer::AsyncLineWriter;
 use color_eyre::eyre::Result;
-use highlighter_builder::groups;
+use highlighter::groups;
 use inlet_manifold::Highlighter;
 use rayon::iter::IntoParallelIterator;
 use theme::reader;
@@ -26,20 +26,13 @@ async fn main() -> Result<()> {
 
     let cli = Cli::parse();
 
-    // let config = config::create_config_or_exit_early(&cli);
     let config = config::create_config(&cli)?;
-
+    let theme = reader::parse_theme(cli.config_path.clone())?;
+    let keywords_from_cli = get_keywords_from_cli(&cli);
     let highlighter_groups = groups::get_highlighter_groups(&cli.enable, &cli.disable)?;
 
-    let new_theme = reader::parse_theme(cli.config_path.clone())?;
-    let keywords_from_cli = get_keywords_from_cli(&cli);
-
-    let highlighter = highlighter_builder::get_highlighter(
-        highlighter_groups,
-        new_theme,
-        keywords_from_cli,
-        cli.no_builtin_keywords,
-    )?;
+    let highlighter =
+        highlighter::get_highlighter(highlighter_groups, theme, keywords_from_cli, cli.no_builtin_keywords)?;
 
     let (reached_eof_tx, reached_eof_rx) = oneshot::channel::<()>();
     let (io, presenter) = get_io_and_presenter(config, Some(reached_eof_tx)).await;
